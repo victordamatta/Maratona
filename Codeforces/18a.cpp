6d2be5f7938f496879a58b9b795906367fe80faa
victormatta <victormatta14@gmail.com>
2016-02-28 20:27:34 -0300

    hacker cup

--- Codeforces/18a.cpp
+++ Codeforces/18a.cpp
@@ -1,42 +1,69 @@
 #include <bits/stdc++.h>
+/*
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <string>
+#include <map>
+*/
 #define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define scd second
+#define f(x, let) for(int let=0; let<x; let++)
+#define ms(x, v) memset(x, v, sizeof x)
 using namespace std;
-typedef int num;
-//REMEMBER TO DECLARE GLOBAL VARIABLES
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef set<int> si;
+typedef set<int>::iterator sit;
+const int MOD = 1000000007;
+const int OO = 0x3f3f3f;
+//REMEMBER LONG LONG INT
+//REMEMBER TO INITIALZE THINGS
+int x[3], y[3];
 
-float x1, y1, x2, y2, x3, y3, m1, m2;
+int isright () {
+    if(x[1] == x[0] && y[1] == y[0]) return 0;
+    if(x[1] == x[2] && y[1] == y[2]) return 0;
+    if(x[2] == x[0] && y[2] == y[0]) return 0;
+    if((x[1] - x[0]) * (x[2] - x[0]) + (y[1] - y[0]) * (y[2] - y[0]) == 0){
+        return 1;
+    }
+    if((x[0] - x[1]) * (x[2] - x[1]) + (y[0] - y[1]) * (y[2] - y[1]) == 0){
+        return 1;
+    }
+    if((x[1] - x[2]) * (x[0] - x[2]) + (y[1] - y[2]) * (y[0] - y[2]) == 0){
+        return 1;
+    }
+    return 0;
+}
 
 int main(){
     std::ios::sync_with_stdio(false);
-    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    if(((y1-y2)/(x1-x2))*((y1-y3)/(x1-x3))==-1)
-        cout << "RIGHT\n";
-    else if(((y2-y3)/(x2-x3))*((y2-y1)/(x2-x1))==-1)
-        cout << "RIGHT\n";
-    else if(((y3-y2)/(x3-x2))*((y3-y1)/(x3-x1))==-1)
-        cout << "RIGHT\n";
-    else if(((y1+1-y2)/(x1-x2))*((y1+1-y3)/(x1-x3))==-1)
-        cout << "ALMOST\n";
-    else if(((y2-y3)/(x2-x3))*((y2-y1+1)/(x2-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y3-y2)/(x3-x2))*((y3-y1+1)/(x3-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y1-1-y2)/(x1-x2))*((y1-1-y3)/(x1-x3))==-1)
-        cout << "ALMOST\n";
-    else if(((y2-y3)/(x2-x3))*((y2-y1-1)/(x2-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y3-y2)/(x3-x2))*((y3-y1-1)/(x3-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y1-y2+1)/(x1-x2))*((y1-y3)/(x1-x3))==-1)
-        cout << "ALMOST\n";
-    else if(((y2+1-y3)/(x2-x3))*((y2+1-y1)/(x2-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y3-y2+1)/(x3-x2))*((y3-y1)/(x3-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y1-y2-1)/(x1-x2))*((y1-y3)/(x1-x3))==-1)
-        cout << "ALMOST\n";
-    else if(((y2-1-y3)/(x2-x3))*((y2-1-y1)/(x2-x1))==-1)
-        cout << "ALMOST\n";
-    else if(((y3-y2-1)/(x3-x2))*((y3-y1)/(x3-x1))==-1)
-        cout << "ALMOST\n";
+    cin >> x[0] >> y[0] >> x[1] >> y[1] >> x[2] >> y[2];
+    if (isright()) cout << "RIGHT\n";
+    else{
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, -1, 0, 1};
+        f(3, i){
+            f(4, j){
+                x[i] += dx[j];
+                y[i] += dy[j];
+                if(isright()){
+                    cout << "ALMOST\n";
+                    return 0;
+                }
+                x[i] -= dx[j];
+                y[i] -= dy[j];
+            }
+        }
+        cout << "NEITHER\n";
+    }
 }
