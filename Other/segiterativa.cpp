bd33ca272044485dd7c2fd836b40ef1dd8da7317
vmatta <victormatta14@gmail.com>
2016-05-11 16:14:24 -0300

    seg iterativa

---
+++ Other/segiterativa.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 200;
+int tree[2*N];
+int n;
+
+void build() {
+    for(int i = n-1; i > 0; i--) tree[i] = max(tree[i<<1], tree[i<<1|1]);
+}
+
+int query(int l, int r) {
+    int ret = 0;
+    for(l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        if(l&1) ret = max(ret, tree[l++]);
+        if(r&1) ret = max(ret, tree[--r]);
+    }
+    return ret;
+}
+
+
+int gcd (int a, int b) {
+    if(b == 0) return a;
+    return gcd(b,a%b);
+}
+
+
+int main () {
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++) scanf("%d",tree+i+n);
+    build();
+    while(1) {
+        int a, b;
+        scanf("%d %d",&a,&b);
+        printf("%d\n",query(a,b+1));
+    }
+}
