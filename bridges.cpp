0d11d6cac2951e7cf24749e9c2aad629ea992770
vmatta <victormatta14@gmail.com>
2017-03-22 19:30:19 -0300

    seletiva upsolving

--- bridges.cpp
+++ bridges.cpp
@@ -23,37 +23,29 @@ map<pi, int> mm;
 int dist[300][300];
 pi vertex[300];
 
-vpi graph[300];
-
 int main() {
     std::ios::sync_with_stdio(false);
     int T;
     cin >> T;
     while (T--) {
         cin >> n >> f >> m;
-        f (n, i) graph[i].clear ();
         mm.clear ();
-        v = 0;
+        v = 1;
         f (n, i) {
             vertex[v] = mp(i, 1);
             mm[mp(i, 1)] = v++;
         }
-        f (n, i) {
-            f (n, j) {
+        f (300, i) {
+            f (300, j) {
                 dist[i][j] = OO;
-                if (i == j) dist[i][j] = 0;
             }
         }
         f (n-1, i) {
             int ui = mm[mp(i, 1)], uj = mm[mp(i+1, 1)];
-            graph[ui].pb (mp(uj, 1));
-            graph[uj].pb (mp(ui, 1));
             dist[ui][uj] = 1;
             dist[uj][ui] = 1;
         }
         int ui = mm[mp(n, 1)], uj = mm[mp(1, 1)];
-        graph[ui].pb (mp(uj, 1));
-        graph[uj].pb (mp(ui, 1));
         dist[ui][uj] = 1;
         dist[uj][ui] = 1;
         f (m, i) {
@@ -67,58 +59,74 @@ int main() {
                 mm[mp(bj, fj)] = v++;
             }
             int ui = mm[mp(bi, fi)], uj = mm[mp(bj, fj)];
+            int basei = mm[mp(bi, 1)], basej = mm[mp(bj, 1)];
 
-            graph[ui].pb (mp (1, fi - 1));
-            graph[1].pb (mp (ui, fi - 1));
-            dist[ui][1] = fi - 1;
-            dist[1][ui] = fi - 1;
-            graph[uj].pb (mp (1, fj - 1));
-            graph[1].pb (mp (uj, fj - 1));
-            dist[uj][1] = fj - 1;
-            dist[1][uj] = fj - 1;
+            dist[ui][basei] = min (dist[ui][basei], fi - 1);
+            dist[basei][ui] = min (dist[basei][ui], fi - 1);
+            dist[uj][basej] = min (dist[uj][basej], fj - 1);
+            dist[basej][uj] = min (dist[basej][uj], fj - 1);
 
-            graph[ui].pb (mp (uj, t));
-            graph[uj].pb (mp (ui, t));
-            dist[ui][uj] = t;
-            dist[uj][ui] = t;
+            dist[ui][uj] = min (dist[ui][uj], t);
+            dist[uj][ui] = min (dist[uj][ui], t);
         }
-        f (v, i) {
-            f (v, j) {
-                f (v, k) {
+
+        f (v-1, i) {
+            f (v-1, j) {
+                int ui = mm[vertex[i]], uj = mm[vertex[j]];
+                if (vertex[i].fst == vertex[j].fst) {
+                    dist[ui][uj] = abs (vertex[i].scd - vertex[j].scd);
+                }
+            }
+        }
+
+        f (v-1, i) {
+            f (v-1, j) {
+                f (v-1, k) {
                     if (dist[i][j] > dist[i][k] + dist[k][j])
                         dist[i][j] = dist[i][k] + dist[k][j];
                 }
             }
         }
 
+        f (v-1, i) {
+            f (v-1, j)
+                cout << dist[i][j] << ' ';
+            cout << endl;
+        }
+        cout << endl;
+
         cin >> q;
         for (int i = 0; i < q; i++) {
             cin >> qbi >> qfi >> qbj >> qfj;
             int ui = OO, uci, uj = OO, ucj, di = -1, dci, dj = -1, dcj;
-            for (int j = 0; j < v; j++) {
-                //cout << vertex[j].fst << ' ' << vertex[j].scd << endl;
+            for (int j = 1; j < v; j++) {
+                /* cout << vertex[j].fst << ' ' << vertex[j].scd << endl; */
                 if (vertex[j].fst == qbi) {
-                    if (vertex[j].scd > qfi && vertex[j].scd < ui) {
+                    if (vertex[j].scd >= qfi && vertex[j].scd < ui) {
                         ui = vertex[j].scd;
                         uci = abs (vertex[j].scd - qfi);
                     }
-                    if (vertex[j].scd < qfi && vertex[j].scd > di) {
+                    if (vertex[j].scd <= qfi && vertex[j].scd > di) {
                         di = vertex[j].scd;
                         dci = abs (vertex[j].scd - qfi);
                     }
                 }
                 if (vertex[j].fst == qbj) {
-                    if (vertex[j].scd > qfj && vertex[j].scd < uj) {
+                    if (vertex[j].scd >= qfj && vertex[j].scd < uj) {
                         uj = vertex[j].scd;
                         ucj = abs (vertex[j].scd - qfj);
                     }
-                    if (vertex[j].scd < qfj && vertex[j].scd > dj) {
+                    if (vertex[j].scd <= qfj && vertex[j].scd > dj) {
                         dj = vertex[j].scd;
                         dcj = abs (vertex[j].scd - qfj);
                     }
                 }
             }
-            cout << ui << ' ' << uci << ' ' << di << ' ' << dci << endl;
+            cout << endl;
+            cout << mm[mp(qbi, qfi)] << " (" << qbi << ' ' << qfi << ") ";
+            cout << "Sup: (" << qbi << ',' << ui << ") -> " << uci << " Inf: (" << qbi << ',' << di << ") -> " << dci << endl;
+            cout << mm[mp(qbj, qfj)] << " (" << qbj << ' ' << qfj << ") ";
+            cout << "Sup: (" << qbj << ',' << uj << ") -> " << ucj << " jnf: (" << qbj << ',' << dj << ") -> " << dcj << endl;
             int ans = OO;
             ans = min (ans, uci + dist[mm[mp (qbi, ui)]][mm[mp(qbj, uj)]] + ucj);
             ans = min (ans, uci + dist[mm[mp (qbi, ui)]][mm[mp(qbj, dj)]] + dcj);
